5839b7d40fb3b02c0e357d010e70d7ab12172ac7
emon <tj.emon33@gmail.com>
2025-02-03 19:09:04 +0600

    making it reusable

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating maxRating={7} />
-    <StarRating />
+    <StarRating message={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating size={24} color="green" className="" />
   </React.StrictMode>
 );
