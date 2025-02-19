744ef240b79a884565ae6968bc5c0faae543aad5
emon <tj.emon33@gmail.com>
2025-02-19 19:27:23 +0600

    component lifecycle

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
-function TestStar() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating
-        color="blue"
-        maxRating={7}
-        handleMovieRating={setMovieRating}
-      />
-      <p>The movie is rated {movieRating} star</p>
-    </div>
-  );
-}
+// function TestStar() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         color="blue"
+//         maxRating={7}
+//         handleMovieRating={setMovieRating}
+//       />
+//       <p>The movie is rated {movieRating} star</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating message={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <App />
+    {/* <StarRating message={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
     <StarRating size={24} color="green" className="" />
-    <TestStar />
+    <TestStar /> */}
   </React.StrictMode>
 );
