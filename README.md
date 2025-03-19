4d849d5e8ae9bff770b3fa7ecb991c871443ddb9
emon <tj.emon33@gmail.com>
2025-03-19 21:53:06 +0600

    dom with useRef

--- README.md
+++ README.md
@@ -147,3 +147,35 @@ useEffect(
 ### useState vs UseRef
 
 ![alt text](ref-vs-state.png)
+
+### Selecting dom element(useRef)
+
+```js
+useEffect(
+  function () {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return function () {
+      document.removeEventListener("keydown", callback);
+    };
+  },
+  [setQuery]
+);
+
+return (
+  <input
+    className="search"
+    type="text"
+    placeholder="Search movies..."
+    value={query}
+    onChange={(e) => setQuery(e.target.value)}
+    ref={inputEl}
+  />
+);
+```
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -305,6 +305,25 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -312,6 +331,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
